35c5596c7e364145713d622091fcabab8ba5cc30
kuhaha <kevin.yr.cheng@gmail.com>
2026-06-01 11:22:04 +0900

    Update page_header.php

--- app/views/layout/page_header.php
+++ app/views/layout/page_header.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html> 
 <html><head>
 <meta http-equiv="Content-TYPE" content="text/html; charset=UTF-8">
-<link rel="stylesheet" type="text/css" href="<?=$appRoot ?>/css/style.css">
-<link rel="stylesheet" type="text/css" href="<?=$appRoot ?>/css/modal.css">
-<title><?=$appName ?></title>
+<link rel="stylesheet" type="text/css" href="<?= $appRoot ?>/css/style.css">
+<link rel="stylesheet" type="text/css" href="<?= $appRoot ?>/css/modal.css">
+<title><?= $appName ?></title>
 </head>
 <body>
 <div class="wrapper">
 <div id="navbar">
-    <div><?=$appName ?> - <?= $userName ?></div>
+    <div><?= $appName ?> - <?= $userName ?></div>
     <div class="push-right">
         <a class="menu-item" href="<?= $appRoot ?>">HOME</a>&nbsp
-<?php foreach($appMenu as $_label => $_action) { ?> 
+    <?php foreach($appMenu as $_label => $_action) { ?> 
     <a class="menu-item" href="<?= $appRoot . $_action ?>"><?= $_label ?></a>&nbsp;
-<?php } ?>
+    <?php } ?>
     </div>
 </div>
