363c0e41f6f2d71ca3ad6425ce7c64a40dc8b7f0
kuhaha <kevin.yr.cheng@gmail.com>
2026-06-02 09:15:37 +0900

    Create Dispatcher class

---
+++ app/Dispatcher.php
@@ -0,0 +1,84 @@
+<?php
+
+class Dispatcher
+{
+    private array $config;
+    private string $model_dir;
+    private string $view_dir;
+    private string $ctrl_dir;
+    private string $app_root;// プロジェクト・ホームディレクトリ, 例：/wp2026mvc
+
+    function __construct(string $app_dir, array $conf, array $server)
+    {
+        $this->config = $conf;
+        $this->config['db'] = $server['db'];
+        $this->model_dir = "models";
+        $this->view_dir  = "views";
+        $this->ctrl_dir  = "controllers"; 
+        $this->app_root = dirname($_SERVER['PHP_SELF']); 
+    }
+
+    /** 
+     リクエストを解析し、MVCモデル構築に必要な情報を抽出する。
+    例：http://www.example.jp/wp2026mvc/u/edit/?id=s0002
+    */
+    private function parseRequest()
+    {
+        $res = [];
+        $defaults = $this->config['mvc']['defaults'];
+        $classes =  $this->config['mvc']['classes'];
+        
+        $path = $_SERVER['REDIRECT_URL'] ?? $_SERVER['REQUEST_URI']; // リスクエストの絶対パス, 例：/wp2026mvc/u/edit/
+        $path = substr($path, strlen($this->app_root)); // リスクエストの相対パス。例： /u/edit/
+        $path = trim($path, '/'); // 前後の'/'を取り除く。例：u/edit
+        $parts = explode('/', $path);   // '/'で区切った部分を抽出し配列にする。例：['u','edit']
+        
+        $c = $parts[0] ?? $defaults['c'];   // クラスIDを抽出する。例：u
+        $res['class'] = $classes[$c] ?? $defaults['class']; // 実際のクラス名を特定する。例：User
+        $res['action'] = $parts[1] ?? $defaults['action'];  // アクションを抽出する。例：edit ※存在しないアクションはデフォルトlistとする
+        $res['args'] = $_GET; // 引数を取得する。例：['id'=>s0002]
+        return $res;
+    }
+
+    /** 
+     解析結果をもとに、MVCモデルを構築し、アクションを呼び出す。
+    */
+    public function run()
+    { 
+        $res = $this->parseRequest();
+        extract($res);//配列から変数を展開。以下3行で行ったことと同じ
+        // $class = $res['class'];
+        // $action = $res['action'];
+        // $args = $res['args'];
+
+        // モデルmodel
+        require "{$this->model_dir}/{$class}Model.php";
+        $modelClass = "{$class}Model";
+        $model = new $modelClass($this->config['db']);
+
+        // ビューview
+        require "{$this->view_dir}/{$class}View.php";
+        $viewClass = "{$class}View";
+        $view = new $viewClass($this->app_root);
+
+        //複数のビュー間に変数(camelCase)をシェアする
+        $view->share('appName', $this->config['app']['name']);
+        $urole = $_SESSION['urole'] ?? 0;
+        $view->share('appMenu', $this->config['menu'][$urole] ?? []);
+        $view->share('userRole', $urole);
+        $view->share('userName', $_SESSION['uname'] ?? 'ゲスト');
+
+        //コントローラーcontroller
+        require "{$this->ctrl_dir}/{$class}Controller.php";
+        $controllerClass = "{$class}Controller";
+        $controller = new $controllerClass($model, $view);
+        $actionFunc = "{$action}Action";
+
+        //アクションを呼び出す（dispatch）
+        if (method_exists($controller, $actionFunc)) {
+            call_user_func_array([$controller, $actionFunc], $args);// メソッドを呼び出す。例：$controller->editAction('s0002');
+        } else {
+            $controller->errorAction("エラー：{$actionFunc}()：未定義アクション！");
+        }
+    }
+}
--- index.php
+++ index.php
@@ -1,64 +1,15 @@
 <?php
 session_start();
+
 //アプリケーション全体の設定ファイルを読み込む
 $conf = require('conf/config.inc.php');
+
 //サーバー別の設定ファイルを読み込む
 // 必要に応じて、片方をコメントアウトして下さい
 $server = require('conf/server-development.inc.php'); //開発用サーバー環境を使用する場合
 // $server = require('conf/server-deployment.inc.php');//公開用サーバー環境を使用する場合
 
 $app_dir = 'app';
-$model_dir = "{$app_dir}/models";
-$view_dir  = "{$app_dir}/views";
-$ctrl_dir  = "{$app_dir}/controllers";
-
-/** 
- リクエストを解析し、MVCモデル構築に必要な情報を抽出する。
- 例：http://www.example.jp/wp2026mvc/u/edit/?id=s0002
-*/
-$defaults = $conf['mvc']['defaults'];
-$classes =  $conf['mvc']['classes'];
-$base = dirname($_SERVER['PHP_SELF']); // プロジェクト・ホームディレクトリ, 例：/wp2026mvc
-
-//クラスとアクションを抽出する
-$path = $_SERVER['REDIRECT_URL']??$_SERVER['REQUEST_URI']; // リスクエストの絶対パス, 例：/wp2026mvc/u/edit/
-$path = substr($path, strlen($base)); // リスクエストの相対パス。例： /u/edit/
-$path = trim($path, '/'); // 前後の'/'を取り除く。例：u/edit
-$parts = explode('/', $path);   // '/'で区切った部分を抽出し配列にする。例：['u','edit']
-$c = $parts[0] ?? $defaults['c'];   // クラスIDを抽出する。例：u
-$class = $classes[$c] ?? $defaults['class']; // 実際のクラス名を特定する。例：User
-$action = $parts[1] ?? $defaults['action'];  // アクションを抽出する。例：edit ※存在しないアクションはデフォルトlistとする
-$args = $_GET; // 引数を取得する。例：['id'=>s0002]
-
-/** 
- 解析結果をもとに、MVCモデルを構築する。
-*/
-// モデルmodel
-require "{$model_dir}/{$class}Model.php";
-$modelClass = "{$class}Model";
-$model = new $modelClass($server['db']);
-
-// ビューview
-require "{$view_dir}/{$class}View.php";
-$viewClass = "{$class}View";
-$view = new $viewClass($base);
-
-//複数のビュー間に変数(camelCase)をシェアする
-$view->share('appName', $conf['app']['name']);
-$urole = $_SESSION['urole'] ?? 0;
-$view->share('appMenu', $conf['menu'][$urole] ?? []);
-$view->share('userRole', $urole);
-$view->share('userName', $_SESSION['uname'] ?? 'ゲスト');
-
-//コントローラーcontroller
-require "{$ctrl_dir}/{$class}Controller.php";
-$controllerClass = "{$class}Controller";
-$controller = new $controllerClass($model, $view);
-$actionFunc = "{$action}Action";
-
-//アクションを呼び出す（dispatch）
-if (method_exists($controller, $actionFunc)) {
-    call_user_func_array([$controller, $actionFunc], $args);// メソッドを呼び出す。例：$controller->editAction('s0002');
-} else {
-    $controller->errorAction("エラー：{$actionFunc}()：未定義アクション！");
-}
+require "{$app_dir}/Dispatcher.php";
+$dispatcher = new Dispatcher($app_dir, $conf, $server);
+$dispatcher->run();
