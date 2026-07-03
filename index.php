f7d69b7fdc3bae01fc989bba7e38c4f04f5356d8
kuhaha <kevin.yr.cheng@gmail.com>
2026-07-04 08:00:09 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ session_start();
 $conf = require('conf/config.inc.php');
 
 //サーバー別の設定ファイルを読み込む。必要に応じて、片方をコメントアウトして下さい
-$server = require('conf/server-development.inc.php');  //開発用サーバーを使用する場合
-// $server = require('conf/server-deployment.inc.php');//公開用サーバーを使用する場合
+//$server = require('conf/server-development.inc.php');  //開発用サーバーを使用する場合
+$server = require('conf/server-deployment.inc.php');//公開用サーバーを使用する場合
 
 require "app/Dispatcher.php";
 $dispatcher = new Dispatcher($conf, $server);
